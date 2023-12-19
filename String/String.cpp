2c02cc63c5d279c3e70ac9fcd273be1db62f2a39
Supchik <eagersupchik@gmail.com>
2023-12-19 18:37:35 +0300

    Добавьте файлы проекта.

---
+++ String/String.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+void ZipText(string g)
+{
+    for (int i = 0; i < size(g) - 1; i++)
+    {
+        if (g[i] == '.' and g[i + 1] == '.')
+        {
+            g.erase(i, 1);
+            i--;
+        }
+            
+
+        if (i == (size(g) - 2) && g[i + 1] == '.')
+        {
+            g.erase(i + 1, 1);
+        }
+    }
+    cout << g;
+}
+
+int main()
+{
+    string g = ".........Hello.....World...!...!..."; //Заменил пробелы на точки, чтобы продемнострировать работу функции
+    ZipText(g);
+}
